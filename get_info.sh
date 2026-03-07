b8fec1b03b122cf036ea5593ca62a388d0e2d6d0
Ramil <ramzeskin@mail.ru>
2026-03-07 17:07:02 +0500

    Read from file v2

--- get_info.sh
+++ get_info.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 file="/etc/hosts"
 cnt=`wc get_info.sh |awk '{print $1}'`
-for ((x=1;x<=cnt;x++));do
-   read line
+while read line;do
    case $line in
      '' ) : ;;
     127*) : ;;
